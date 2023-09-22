094cc2448399353ae839665ae507fef767d2259e
Mikhal Angelo Misenas <market.chugger@gmail.com>
2023-09-22 11:21:00 -0700

    text-based adventure game file

---
+++ advgame.py
@@ -0,0 +1,13 @@
+print('Welcome to the Haunted Mansion')
+print('You are a distant family member of the rich millionaire who lived in the mansion, leaving the mansion to you')
+print('Do you want to enter the mansion?')
+
+###Prompt User for a Choice
+entryChoice = input('>')
+
+if(entryChoice == 'Yes'):
+    print('You entered the mansion, and found it empty')
+elif(entryChoice == 'No'):
+    print('You walk back to your car')
+else:
+    print('That is not one of the valid choices, please pick Yes or No')
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 animals = ["dog", "cat", "bird", "fish"]
+
+//Sept20,2023
+/*
+To-Dos: 
+    connect to a nodejs server
+    add footer section
+    write code for at least 45mins
+    evaluate code and take notes
+    
+*/
+
+console.log('Connected..')
